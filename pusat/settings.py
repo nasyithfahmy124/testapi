828c5a677823263bae4f6e937df0443a5ab6d5fb
Nasyith Fahmy <nasyithfahmy124@gmail.com>
2026-05-14 20:28:54 +0700

    final

--- pusat/settings.py
+++ pusat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','.netlify.app']
+ALLOWED_HOSTS = ['*','.netlify.app','.onrender.com']
 
 
 # Application definition
